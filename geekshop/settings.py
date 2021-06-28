8fcc58f4b8de745199d919954b6b818de8bfc944
mifrok <mifra@mail.ru>
2021-06-29 06:31:37 +0700

    lesson_3_add_photo

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xxac+ln+74ue7w1tj7%l3kw)9ygh893s6q$g)5ps4c#s!=19yk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
